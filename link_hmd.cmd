60d3f0830834652500727694d050c59b71d9bf72
rui_fang <rui_fang@htc.com>
2017-02-15 15:47:57 +0800

    add common path

--- link_hmd.cmd
+++ link_hmd.cmd
@@ -1,15 +1,17 @@
 @echo off
-set projectPath=..\Link\link_hmd\SYS\EWARM
+set MyProjectPath=..\Link\link_hmd
+
+set projectPath=%MyProjectPath%\SYS\EWARM
 set projectName=link_sys.ewp
 set buildType=Release
 set startAddr=0x08040000
 if .%1==.bl0 (
-	set projectPath=..\Link\link_hmd\BL0\EWARM
+	set projectPath=%MyProjectPath%\BL0\EWARM
 	set projectName=link_BL0.ewp
 	set startAddr=0x08000000
 )
 if .%1==.bl1 (
-	set projectPath=..\Link\link_hmd\BL1\EWARM
+	set projectPath=%MyProjectPath%\BL1\EWARM
 	set projectName=link_BL1.ewp
 	set startAddr=0x08008000
 )
