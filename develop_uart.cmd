662ad44dc06692023778ba5ff3c44b348cb298e0
rui_fang <rui_fang@htc.com>
2017-05-08 10:12:46 +0800

    优化 develop_uart.cmd

--- develop_uart.cmd
+++ develop_uart.cmd
@@ -1,3 +1,4 @@
+setlocal enabledelayedexpansion
 set MyprojectPath=%1
 set MyProjectName=%2
 set MybuildType=%3
@@ -6,7 +7,10 @@ set MyName=%MyProjectName:~0,-4%
 set MyFilePath=%MyprojectPath%\%MybuildType%\Exe
 set MyComPort=%5
 set MyComNum=%MyComPort:~3%
-set MyCommand=pm reboot dfu\r\n
+set MyCommand=pm reboot sysdfu
+set /a regnum=0
+
+set StressTest=true
 
 ::IarBuild %MyprojectPath%\%MyProjectName% -build %MybuildType% -log all
 IarBuild %MyprojectPath%\%MyProjectName% -make %MybuildType% -log errors
@@ -41,12 +45,52 @@ if "%postfix%" == ":" (
 :ENTER_DFU
 set MyBaudRate=115200
 mode %MyComPort%:BAUD=%MyBaudRate% DATA=8 PARITY=N dtr=off rts=off
-::echo %MyCommand%>%MyComPort%
+echo %MyCommand% > %MyComPort%
 
-
-
-set MyDownloadPar=-c --pn %MyComNum% --br %MyBaudRate% -i STM32F4_11_512K
-set MySector=-d --auto --v --a
-.\FlashLoaderForUart\HTC\HTCFlashLoader.exe %MyDownloadPar% %MySector% %MystartAddr% --fn %MyFilePath%\%MyName%.bin -r --a %MystartAddr%
-
+::set MyBaudRate=57600
+set TestVsion=3
+if %TestVsion%==1 (
+	echo Tool version: HTCFlashLoader v1.0.1
+	set MyDownloadPar=-c --pn %MyComNum% --br %MyBaudRate% -i STM32F4_11_512K
+	set MySector= -d --auto --v --a
+	.\FlashLoaderForUart\My\HTCFlashLoader.exe !MyDownloadPar! !MySector! %MystartAddr% --fn %MyFilePath%\%MyName%.bin -r --a 0x08000000
+	set MyReturn=errorlevel
+)
+if %TestVsion%==2 (
+	echo Tool version: HTCFlashLoader v1.0.0
+	set %MyDownloadPar%=-c --pn %MyComNum% --br %MyBaudRate% -i STM32F4_11_512K
+	if %MystartAddr%==0x08000000 set MySector= -d --sec 1 0 --v --a
+	if %MystartAddr%==0x08008000 set MySector= -d --sec 3 2 3 4 --v --a
+	if %MystartAddr%==0x08040000 set MySector= -d --sec 2 6 7 --v --a
+	.\FlashLoaderForUart\HTC\HTCFlashLoader.exe !MyDownloadPar! !MySector! %MystartAddr% --fn %MyFilePath%\%MyName%.bin -r --a 0x08000000
+	set MyReturn=errorlevel
+)
+if %TestVsion%==3 (
+	echo Tool version: STMFlashLoader
+	if %MystartAddr%==0x08000000 set MyDownloadPar=-c --pn %MyComNum% --br %MyBaudRate% -i STM32F4_11_512K -e --sec 1 0
+	if %MystartAddr%==0x08008000 set MyDownloadPar=-c --pn %MyComNum% --br %MyBaudRate% -i STM32F4_11_512K -e --sec 3 2 3 4
+	if %MystartAddr%==0x08040000 set MyDownloadPar=-c --pn %MyComNum% --br %MyBaudRate% -i STM32F4_11_512K -e --sec 2 6 7
+	set MySector= -d --v --a
+	.\FlashLoaderForUart\STM\STMFlashLoader.exe !MyDownloadPar! !MySector! %MystartAddr% --fn %MyFilePath%\%MyName%.bin -r --a 0x08000000
+	set MyReturn=errorlevel
+)
+If %MyReturn%==1 (
+	echo=
+	echo ---------------------------
+	echo   Flash loader failed!!!  
+	echo ---------------------------
+	echo=
+	pause
+) Else (
+	if %StressTest%==true (
+		echo=
+		set /a regnum+=1
+		echo Test count :  %regnum%
+		echo Please wait 5s to restart test ...
+		echo=
+		ping -n 4 127.0.0.1 > temp
+		del temp
+		goto ENTER_DFU
+	)
+)
 :END
